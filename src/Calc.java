fca1867c8fd3e3f2beae62ab54d0e9fd9924dd64
lyutip <markeijob@gmail.com>
2023-11-21 13:32:27 +0300

    До меня дошло, очень стыдно. Все изсправил

--- src/Calc.java
+++ src/Calc.java
@@ -39,11 +39,11 @@ public class Calc {
             return false;
     }
     static String getNameForValue(int value) {
-                int[] answerArab = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+                int[] answerArab = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 49, 50, 54, 56, 60, 63, 64, 70, 72, 80, 81, 90, 100};
                 String[] answerRom = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI",
-                        "XVII", "XVIII", "XIX", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
-                int i = 0;
-                for( ; i < answerArab.length; i++) {
+                        "XVII", "XVIII", "XIX", "XX", "XXI", "XXIV", "XXV", "XXVII", "XXVIII", "XXX", "XXXII", "XXXV", "XXXVI", "XL", "XLII", "XLV", "XLVIII", "XLIX", "L", "LIV", "LVI", "LX", "LXIII", "LXIV", "LXX", "LXXII", "LXXX", "LXXXI", "XC", "C"};
+
+                for(int i = 0; i < answerArab.length; i++) {
                     if (value == answerArab[i])
                         return answerRom[i];
                 }
